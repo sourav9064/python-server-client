2265e0d07a9fea042028011b3097b1c722dbf96e
Sourav Das <sdas121196@gmail.com>
2021-03-14 10:46:06 +0530

    Add files via upload

---
+++ 1_client.py
@@ -0,0 +1,29 @@
+# running on Client
+
+import cv2
+import zmq
+import base64
+import numpy as np
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.SUB)
+footage_socket.bind('tcp://*:5555')
+#footage_socket.bind('192.168.1.0:5555')
+footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
+
+while True:
+    try:
+        frame = footage_socket.recv_string()
+        img = base64.b64decode(frame)
+        npimg = np.fromstring(img, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+        cv2.imshow("Stream", source)
+        cv2.waitKey(1)
+
+    except KeyboardInterrupt:
+    #except:
+    #    k = cv2.waitKey(1)
+    #    if k == ord('q'):
+    #        break
+        cv2.destroyAllWindows()
+        break
